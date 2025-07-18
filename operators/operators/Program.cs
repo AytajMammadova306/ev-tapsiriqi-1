f98253bf691e5349f92385173c2a39526fb12148
AytajMammadova306 <aytajamam@code.edu.az>
2025-07-18 21:10:50 +0400

    homework

---
+++ operators/operators/Program.cs
@@ -0,0 +1,51 @@
+﻿//task1
+
+
+//int num1 = 5;
+//int num2 = 6;
+//int num3 = 6;
+//if (num1 >= num2 && num1 >= num3)
+//{
+//    Console.WriteLine(num1);
+//}
+//else if (num2 >= num1 && num2 >= num3)
+//{
+//    Console.WriteLine(num2);
+//}
+//else if (num3 >= num1 && num3 >= num2)
+//{
+//    Console.WriteLine(num3);
+//}
+
+
+//task2
+
+//int yekun = 84;
+//if (yekun < 65)
+//{
+//    Console.WriteLine("kesilmisiniz");
+//}
+//else if (yekun>=65 && yekun <= 84)
+//{
+//    Console.WriteLine("Adi Diplom alib");
+//}
+//else if (yekun>= 85 && yekun <= 94)
+//{
+//    Console.WriteLine("Seref Diplomu alib");
+//}
+//else if (yekun>=95 && yekun <= 100)
+//{
+//    Console.WriteLine("Yuksek Seref Diplomu Alib");
+//}
+//else
+//{
+//    Console.WriteLine("qiymeti sehv daxil etmisiniz");
+//}
+
+//task3
+
+//int num5 = 89;
+//int teklik = num5 % 10;
+//int onluq = num5 / 10;
+//int cem = onluq + teklik;
+//Console.WriteLine(cem);
